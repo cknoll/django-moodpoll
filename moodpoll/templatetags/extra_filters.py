b56353660af12b78745c0a4d5e1300925fbc74b4
Carsten Knoll <carsten.knoll@posteo.de>
2020-03-28 19:22:10 +0100

    introduce bleach for secure handling of markdown generated html

--- moodpoll/templatetags/extra_filters.py
+++ moodpoll/templatetags/extra_filters.py
@@ -1,13 +1,17 @@
 from django import template
 import markdown
+import bleach
 # from moodpoll.release import __version__
 
 register = template.Library()
+bleach.sanitizer.ALLOWED_TAGS += ["p"]
 
 
 @register.filter
-def render_markdown(txt):
-    # !!! here we shoud check if there is no harmfull code in txt
+def render_markdown_bleach(txt):
+    # !!! here we should check if there is no harmfull code in txt
     # because we will pass it through `safe` later
     # however it should be possible to discuss about code (even javascript)
-    return markdown.markdown(txt)
+    return bleach.clean(markdown.markdown(txt))
+
+
